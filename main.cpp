ae2ddf9ee2ace28f103bdbacb40271044f75272a
Moyo <moyo@moyomo.xyz>
2025-01-15 13:15:30 +0100

    Initial setup

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "UI.h"
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
+{
+    #pragma comment(lib, "winmm.lib") // for timeBeginPeriod
+    timeBeginPeriod(1);
+
+    UI::Render();
+
+    return 0;
+}
