d06d085d3418f7ef3550f8ee3bba65a62ea6e09b
Moyo <moyo@moyomo.xyz>
2025-04-03 21:34:03 +0200

    Added mutex check to prevent multiple instances being open at the same time

--- main.cpp
+++ main.cpp
@@ -3,8 +3,22 @@
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
 {
-    #pragma comment(lib, "winmm.lib") // for timeBeginPeriod
-    // Since Windows 10 version 2004 this doesn't affect the global timer resolution anymore
+    // create mutex to make sure only one instance can be open at a time
+    HANDLE mutex = CreateMutexA(NULL, TRUE, "FC2Toverlay");
+    if (GetLastError() == ERROR_ALREADY_EXISTS)
+    {
+        MessageBox(
+            NULL,
+            (LPCWSTR)L"There is another instance of the overlay already running.\nClose the other instance before launching the overlay again.",
+            (LPCWSTR)L"Overlay already open",
+            MB_ICONWARNING | MB_OK
+        );
+        return -1;
+    }
+
+    // for timeBeginPeriod
+    #pragma comment(lib, "winmm.lib")
+    // since Windows 10 version 2004 this doesn't affect the global timer resolution anymore
     timeBeginPeriod(1);
 
     // create settings window
@@ -13,6 +27,9 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
     // create overlay window
     if (Config::bCreateOverlay)
         UI::RenderOverlay();
+
+    // close mutex so new instances can get launched
+    CloseHandle(mutex);
 
     return 0;
 }
